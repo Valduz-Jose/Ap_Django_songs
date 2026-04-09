6a3d9b48c4a7c6ec301db8307d971b96f594e98a
Jose Valduz <jose.valduz@unet.edu.ve>
2026-04-09 12:42:06 -0400

    Fase 3: creacion del modelo, migraciones y superuser

--- canciones/models.py
+++ canciones/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Cancion(models.Model):
+    titulo = models.CharField(max_length=100, verbose_name="Título")
+    artista = models.CharField(max_length=100, verbose_name="Artista")
+    popularidad = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        verbose_name="Popularidad (1-10)"
+    )
+
+    class Meta:
+        db_table = 'cancion' # Nombre exacto de la tabla en MySQL
+        verbose_name = 'Canción'
+        verbose_name_plural = 'Canciones'
+
+    def __str__(self):
+        return f"{self.titulo} - {self.artista}"
